27028ec0722b56cd14f7f7c1a76be25a915acd73
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-04-06 21:52:58 -0300

    adding new restaurant, vegan garden

--- src/mocks/shops.ts
+++ src/mocks/shops.ts
@@ -8,7 +8,7 @@ const items = [
         title: "Tasty",
         image: mediaPath + "place-tasty.webp",
         tag: false,
-        vegan: true,
+        vegan: false,
         meals: ["Desserts"],
         description: "Indulge your sweet tooth at our dessert haven, where every bite is a heavenly delight! From decadent cakes to creamy gelato, our shop is a paradise for dessert lovers. Satisfy your cravings with our irresistible treats crafted with love and passion. Whether you're celebrating a special occasion or simply craving a sweet escape, our diverse menu promises to tantalize your taste buds. Come taste the magic at our dessert shop and experience a symphony of flavors that will leave you craving for more!",
         rating: 4.5,
@@ -210,6 +210,72 @@ const items = [
         description: "Craving a quick bite that won't break the bank? Look no further than our vibrant food truck parked right on the bustling streets! At our mobile kitchen, we serve up a mouthwatering array of snacks and fast food favorites, from sizzling hot dogs to crispy French fries, all at wallet-friendly prices. Whether you're on the go or simply seeking a savory pick-me-up, our convenient location and speedy service make us the perfect pit stop for hungry urban adventurers. Join the food truck frenzy and treat yourself to delicious eats without the fuss – because great taste shouldn't come with a hefty price tag!",
         rating: 4.3,
         totalReviews: 12,
+        menu: [
+            {
+                id: 1,
+                title: "Chocolate Cake",
+                image: mediaPath,
+                type: "Lunch",
+                rating: 4.5,
+                mealInfo: "asdasdsadasdasdasdas",
+                price: 12.3,
+                previousPrice: 15.8
+            },
+            {
+                id: 2,
+                title: "Chocolate Cake",
+                image: mediaPath,
+                type: "Lunch",
+                rating: 4.3,
+                mealInfo: "asdasdsadasdasdasdas",
+                price: 12.3,
+                previousPrice: null
+            },
+            {
+                id: 3,
+                title: "Chocolate Cake",
+                image: mediaPath,
+                type: "Lunch",
+                rating: 4.8,
+                mealInfo: "asdasdsadasdasdasdas",
+                price: 12.3,
+                previousPrice: null
+            }
+        ],
+        reviews: [
+            {
+                id: 1,
+                name: "Jack",
+                photo: mediaPath,
+                stars: 4,
+                comment: "asdasdasdasdsa"
+            },
+            {
+                id: 2,
+                name: "Sarah",
+                photo: mediaPath,
+                stars: 5,
+                comment: "asdasdasdasdsa"
+            },
+            {
+                id: 3,
+                name: "Monica",
+                photo: mediaPath,
+                stars: 5,
+                comment: "asdasdasdasdsa"
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Vegan Garden",
+        image: mediaPath + "place-vegangarden.webp",
+        tag: false,
+        vegan: true,
+        meals: ["Breakfast", "Lunch", "Desserts"],
+        description: "Nestled within the serene embrace of nature lies our vegan oasis, a peaceful retreat where freshness and flavor flourish in harmony. Situated in the heart of a beautiful park, our restaurant invites you to unwind amidst lush greenery and crisp, clean air while savoring delectable plant-based cuisine. From vibrant salads bursting with garden-fresh produce to hearty plant-based burgers that satisfy every craving, our menu is a celebration of health and sustainability. Whether you're a committed vegan or simply seeking a nourishing meal in tranquil surroundings, our haven offers a sanctuary for mindful dining and wholesome indulgence. Join us in embracing the beauty of nature and the goodness of vegan fare – because here, every bite is a step towards wellness and blissful harmony.",
+        rating: 4.7,
+        totalReviews: 17,
         menu: [
             {
                 id: 1,
